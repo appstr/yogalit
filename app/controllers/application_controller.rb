55138a2a1957d9defed985895d0a377a15fac2cd
Christopher Thomas Pelnar <christopherpelnar@gmail.com>
2017-06-27 14:06:00 -0400

    Small changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_action :not_blocked_or_blacklisted?
+  Ahoy.geocode = :async
 
   def split_start_time
     hour = []
